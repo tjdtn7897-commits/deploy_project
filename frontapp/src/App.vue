0cc3925b9dbdaf5b9857c8ec55d97c3ef7d1b635
kimteayoon <kim@youtu.com>
2026-04-06 16:51:20 +0900

    20260406/메인페이지-헤더변경/TY

--- frontapp/src/App.vue
+++ frontapp/src/App.vue
@@ -1,12 +1,18 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { RouterLink, RouterView } from "vue-router";
+import HelloWorld from "./components/HelloWorld.vue";
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-    <h1>Github Actions</h1>
+    <img
+      alt="Vue logo"
+      class="logo"
+      src="@/assets/logo.svg"
+      width="125"
+      height="125"
+    />
+    <h1>Yedam Project</h1>
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
 
